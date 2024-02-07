730aac79678d6a927cc6281481a2c6406b415d1c
roctbb <roctbb@gmail.com>
2024-02-07 14:41:40 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo pip3 install -r requirements.txt
+sudo cp contec.ini /etc/nginx/apps/
 sudo cp agents_contec.conf /etc/supervisor/conf.d/
 sudo cp agents_contec_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
